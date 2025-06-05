3079c8632be159ab4dfa9147b984c634e6e6f9fe
maiva32 <maivandai2812@gmail.com>
2025-06-05 20:18:26 +0700

    Update UserManager.h

--- UserManager.h
+++ UserManager.h
@@ -15,6 +15,9 @@ private:
     bool isUsernameExists(const std::string& username);
     void sendLoginInfoToUser(const std::string& email, const std::string& username, 
                             const std::string& password);
+User currentUser;
+bool isLoggedIn = false;
+
 
 public:
     UserManager(const std::string& usersPath);
@@ -29,3 +32,14 @@ public:
     bool login(const std::string& username, const std::string& password);
     std::string generateRandomPassword();
 }; 
+bool isUserLoggedIn() const { return isLoggedIn; }
+User getCurrentUser() const { return currentUser; }
+
+void setCurrentUser(const User& user) {
+    currentUser = user;
+    isLoggedIn = true;
+}
+
+void logout() {
+    isLoggedIn = false;
+}
