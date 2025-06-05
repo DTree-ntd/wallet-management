59c05758700aa391af349135c800b1603e4c5fd8
maiva32 <maivandai2812@gmail.com>
2025-06-05 22:32:53 +0700

    Update OTPManager.h

--- OTPManager.h
+++ OTPManager.h
@@ -1,17 +1,24 @@
 #ifndef OTP_MANAGER_H
 #define OTP_MANAGER_H
 
-#pragma once
-#include <string>
-#include <map>
-#include <chrono>
+include <string>
+#include <unordered_map>
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
 
 class OTPManager {
 private:
-    std::map<std::string, std::pair<std::string, std::chrono::steady_clock::time_point>> otpStore;
-    const int expirySeconds = 300; // OTP có hiệu lực trong 5 phút
+    std::unordered_map<std::string, std::pair<std::string, time_t>> otpStore;
+    int expirySeconds = 120; // OTP hết hạn sau 2 phút
 
 public:
+    OTPManager() {
+        srand(static_cast<unsigned int>(time(nullptr)));
+    }
+
     std::string generateOTP(const std::string& username);
     bool verifyOTP(const std::string& username, const std::string& otpCode);
 };
+
+#endif
