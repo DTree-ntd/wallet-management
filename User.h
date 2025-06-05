f1ef3789e7eb7dc2a9d323fa79b53d83462bc5f4
maiva32 <maivandai2812@gmail.com>
2025-06-05 21:53:53 +0700

    Update User.h

--- User.h
+++ User.h
@@ -1,44 +1,19 @@
-#ifndef USER_H
-#define USER_H
-
+#pragma once
 #include <string>
-#include <vector>
+#include <unordered_map>
+#include <ctime>
 
-class User {
+class OTPManager {
 private:
-    std::string username;
-    std::string password;
-    std::string fullName;
-    std::string email;
-    std::string phoneNumber;
-    bool isAdmin;
-    bool isPasswordAutoGenerate;
-    std::vector<std::string> walletIds;
+    struct OTPInfo {
+        std::string otp;
+        std::time_t generatedTime;
+    };
+
+    std::unordered_map<std::string, OTPInfo> otpStore;
+    const int expirySeconds = 300; // OTP có hiệu lực 5 phút
 
 public:
-    // Constructors
-    User();
-    User(const std::string& username, const std::string& password, 
-         const std::string& fullName, const std::string& email,
-         const std::string& phoneNumber, bool isAdmin = false,
-         bool isPasswordAutoGenerate = false);
-
-    // Getters
-    std::string getUsername() const;
-    std::string getPassword() const;
-    std::string getFullName() const;
-    std::string getEmail() const;
-    std::string getPhoneNumber() const;
-    bool getIsAdmin() const;
-    bool getIsPasswordAutoGenerate() const;
-    std::vector<std::string> getWalletIds() const;
-
-    // Setters
-    void setPassword(const std::string& newPassword);
-    void setFullName(const std::string& newFullName);
-    void setEmail(const std::string& newEmail);
-    void setPhoneNumber(const std::string& newPhoneNumber);
-    void addWalletId(const std::string& walletId);
+    std::string generateOTP(const std::string& username);
+    bool verifyOTP(const std::string& username, const std::string& otpCode);
 };
-
-#endif 
