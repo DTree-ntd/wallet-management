83b6c7c882d92621a59530ad685e85fa178ff4f3
maiva32 <maivandai2812@gmail.com>
2025-06-05 21:43:47 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <limits>
+#include "OTPManager.h"
 
 void clearScreen() {
     #ifdef _WIN32
@@ -50,21 +51,29 @@ void registerUser(UserManager& userManager) {
     }
 }
 
-void login(UserManager& userManager) {
+void login(UserManager& userManager, OTPManager& otpManager) {
     clearScreen();
     std::string username, password;
     
     std::cout << "\n=== DANG NHAP ===\n";
-    
     std::cout << "Nhap ten dang nhap: ";
     std::cin >> username;
-    
     std::cout << "Nhap mat khau: ";
     std::cin >> password;
     
     if (userManager.login(username, password)) {
-        std::cout << "Dang nhap thanh cong!\n";
-        // TODO: Add user menu here
+        std::string otp = otpManager.generateOTP(username);
+        std::cout << "Ma OTP cua ban la: " << otp << "\n"; // Thay vì gửi email, in ra console
+        std::cout << "Nhap ma OTP: ";
+        std::string inputOtp;
+        std::cin >> inputOtp;
+
+        if (otpManager.verifyOTP(username, inputOtp)) {
+            std::cout << "Dang nhap thanh cong (da xac thuc OTP)!\n";
+            // TODO: Menu nguoi dung dang nhap thanh cong
+        } else {
+            std::cout << "OTP khong hop le hoac het han.\n";
+        }
     } else {
         std::cout << "Dang nhap that bai! Vui long kiem tra lai thong tin.\n";
     }
@@ -72,18 +81,19 @@ void login(UserManager& userManager) {
 
 int main() {
     UserManager userManager("../data/users.txt");
+    OTPManager otpManager;
     int choice;
-    
+
     while (true) {
         showMenu();
         std::cin >> choice;
-        
+
         switch (choice) {
             case 1:
                 registerUser(userManager);
                 break;
             case 2:
-                login(userManager);
+                login(userManager, otpManager);
                 break;
             case 3:
                 clearScreen();
@@ -92,11 +102,10 @@ int main() {
             default:
                 std::cout << "Lua chon khong hop le! Vui long chon lai.\n";
         }
-        
+
         std::cout << "\nNhan Enter de tiep tuc...";
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         std::cin.get();
     }
-    
     return 0;
-} 
+}
